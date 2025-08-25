0bbb0ea08f23b93dc776066eb55829aa0a9cfbf9
Akash Kumar <akashkumarar2003@gmail.com>
2025-08-25 14:05:04 +0200

    Initial commit - Hello World Flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World! 🚀"
+
+if __name__ == "__main__":
+    app.run(host="95.111.231.254", port=5000)
+
